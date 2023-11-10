c098575b30c409abe6089457f459108875b3eba9
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2023-11-10 13:22:19 -0500

    voir pres de

---
+++ public/assets/js/magnify.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    magnifyGlass();
+};
+
+function magnifyGlass() {
+    const img = document.querySelector('.magnify');
+    //const image = img
+    img.addEventListener('mousemove', function(e) {
+        e.preventDefault();
+        const glass = document.querySelector('.glass');
+        glass.style.display = 'block';
+        glass.style.position = 'absolute';
+        glass.style.width = '250px';
+        glass.style.height = '250px';
+        glass.style.borderRadius = '50%';
+        glass.style.backgroundImage = 'url(' + img.src + ')';
+        
+        glass.style.backgroundRepeat = 'no-repeat';
+        glass.style.backgroundOrigin = '0 0';
+        glass.style.backgroundSize = '550px 550px';
+        glass.style.backgroundPosition = '0 0';
+
+        glass.style.boxShadow = '0 0 0 7px rgba(0,0,0,0.2)';
+        glass.style.cursor = 'none';
+        glass.style.left = e.pageX - 50 + 'px';
+        glass.style.top = e.pageY - 50 + 'px';
+        glass.style.transform = 'translate(50%, -50%)';
+
+        const x = e.pageX - this.offsetLeft;
+        const y = e.pageY - this.offsetTop;
+
+        
+        glass.style.backgroundPositionX = -x + 'px';
+        glass.style.backgroundPositionY = -y + 'px';
+
+
+    });
+    img.addEventListener('mouseleave', function() {
+        const glass = document.querySelector('.glass');
+
+        glass.style.display = 'none';
+    });
+
+}
