e7cfb50fa0a19f1aeb46d1b5edbb7b09d0781a6d
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2023-11-10 13:28:41 -0500

    ajustement pres de

--- public/assets/js/magnify.js
+++ public/assets/js/magnify.js
@@ -10,21 +10,21 @@ function magnifyGlass() {
         const glass = document.querySelector('.glass');
         glass.style.display = 'block';
         glass.style.position = 'absolute';
-        glass.style.width = '250px';
-        glass.style.height = '250px';
+        glass.style.width = '400px';
+        glass.style.height = '400px';
         glass.style.borderRadius = '50%';
         glass.style.backgroundImage = 'url(' + img.src + ')';
         
         glass.style.backgroundRepeat = 'no-repeat';
         glass.style.backgroundOrigin = '0 0';
-        glass.style.backgroundSize = '550px 550px';
+        glass.style.backgroundSize = '650px 650px';
         glass.style.backgroundPosition = '0 0';
 
         glass.style.boxShadow = '0 0 0 7px rgba(0,0,0,0.2)';
         glass.style.cursor = 'none';
-        glass.style.left = e.pageX - 50 + 'px';
-        glass.style.top = e.pageY - 50 + 'px';
-        glass.style.transform = 'translate(50%, -50%)';
+        glass.style.left = e.pageX - 25 + 'px';
+        glass.style.top = e.pageY - 25 + 'px';
+        glass.style.transform = 'translate(30%, -30%)';
 
         const x = e.pageX - this.offsetLeft;
         const y = e.pageY - this.offsetTop;
